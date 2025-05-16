0983d34ec83afee34f2f42153a6cf63fe71f942b
kimdlopez <iamkimdlopez@gmail.com>
2025-05-16 16:00:57 +0800

    add before each function

--- cypress/e2e/app-text.cy.js
+++ cypress/e2e/app-text.cy.js
@@ -1,6 +1,9 @@
 describe("App Text", () => {
-  it("should have correct App Title", () => {
+  beforeEach(() => {
     cy.visit("/");
+  });
+
+  it("should have correct App Title", () => {
     cy.get('[data-testid="app-title"]').should(
       "have.text",
       "Learn Automated Testing"
@@ -8,7 +11,6 @@ describe("App Text", () => {
   });
 
   it("should have correct Hero Title", () => {
-    cy.visit("/");
     cy.get('[data-testid="hero-title"]').should(
       "have.text",
       "Let us learn automated web testing!"
@@ -16,7 +18,6 @@ describe("App Text", () => {
   });
 
   it("should display the Login Button", () => {
-    cy.visit("/");
     cy.get('[data-testid="login-link"]')
       .should("exist")
       .and("have.text", "Log In")
